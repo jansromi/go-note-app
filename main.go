7da33a80bb3cd4c0a1b3a4125cf91ca808f7d280
jansromi <rrobertjansson@gmail.com>
2024-02-15 19:27:22 +0200

    show actual data with tui

--- main.go
+++ main.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-	// tui "tui_example/tui"
 	core "go-note-app/core"
-	// tea "github.com/charmbracelet/bubbletea"
+	tui "go-note-app/tui"
+
+	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
 	app := core.NewApp()
-	app.AddItem("title", "desc", "content")
-	// m := tui.NewListPage()
-	// p := tea.NewProgram(m, tea.WithAltScreen())
+	m := tui.NewListPage(app)
+	p := tea.NewProgram(m, tea.WithAltScreen())
 
-	// _, error := p.Run()
-	// if error != nil {
-	// 	panic(error)
-	// }
+	_, error := p.Run()
+	if error != nil {
+		panic(error)
+	}
 
 }
